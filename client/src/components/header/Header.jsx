ef21cd7d3b781682059f28de4f96607247d10638
Nihad <nihadishi@gmail.com>
2023-10-03 02:56:44 +0400

    navbar line

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+import "./style.scss"
+const Header = () => {
+  return (
+    <div className='header'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Header
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./style.scss"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 const Navbar = () => {
   const navigate = useNavigate();
+  const [pathname, setpathname] = useState("")
+  useEffect(() => {
+     setpathname(window.location.pathname) ;
+  }, [pathname])
+  console.log(pathname);
   return (
     <div className='Navbar'>
         <ul>
-            <li onClick={()=>{navigate("/")}}>Home</li>
-            <li onClick={()=>{navigate("/about")}}>About</li>
-            <li onClick={()=>{navigate("/cv")}}>Curriculum Vitae</li>
-            <li onClick={()=>{navigate("/portfolio")}}>Portfolio</li>
-            <li onClick={()=>{navigate("/contact")}}>Contact</li>
+        <li className={pathname === '/' ? 'active' : ''} onClick={() => { navigate("/") }}>Home</li>
+        <li className={pathname === '/about' ? 'active' : ''} onClick={() => { navigate("/about") }}>About</li>
+        <li className={pathname === '/cv' ? 'active' : ''} onClick={() => { navigate("/cv") }}>CV</li>
+        <li className={pathname === '/portfolio' ? 'active' : ''} onClick={() => { navigate("/portfolio") }}>Portfolio</li>
+        <li className={pathname === '/contact' ? 'active' : ''} onClick={() => { navigate("/contact") }}>Contact</li>
         </ul>
     </div>
   )
