6cf2fc5ec5eb45707783ac0f50e6c4d04e04a110
Nihad <nihadishi@gmail.com>
2023-10-05 15:43:19 +0400

    skills start

--- client/src/pages/About/AboutPage.jsx
+++ client/src/pages/About/AboutPage.jsx
@@ -13,22 +13,44 @@ const AboutPage = () => {
         <div className="AboutC">
           <div className="AboutCMe">
             <div className="AboutCMe-left">
-            <div className="AboutCMe-left-name">About</div>
-            <div className="AboutCMe-left-speciality">Full stack developer</div>
-            <div className="AboutCMe-left-about">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium perferendis eum voluptatem laborum, beatae recusandae ex earum ducimus? Quaerat consequuntur repudiandae voluptatum praesentium! Amet nihil sunt repellendus, ab pariatur veniam.</div>
-            <div className="AboutCMe-left-contact">
-              <ul>
-                <li><img src={PhoneSvg} alt="" /><p>+994776373838</p></li>
-                <li><img src={MailSvg} alt="" /><p>nihadishi@gmail.com</p></li>
-                <li><img src={LocationSvg} alt="" /><p>Baku, Azerbaijan</p></li>
-              </ul>
-            </div>
+              <div className="AboutCMe-left-name">About</div>
+              <div className="AboutCMe-left-speciality">
+                Full stack developer
+              </div>
+              <div className="AboutCMe-left-about">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium perferendis eum voluptatem laborum, beatae
+                recusandae ex earum ducimus? Quaerat consequuntur repudiandae
+                voluptatum praesentium! Amet nihil sunt repellendus, ab pariatur
+                veniam.
+              </div>
+              <div className="AboutCMe-left-contact">
+                <ul>
+                  <li>
+                    <img src={PhoneSvg} alt="" />
+                    <p>+994776373838</p>
+                  </li>
+                  <li>
+                    <img src={MailSvg} alt="" />
+                    <p>nihadishi@gmail.com</p>
+                  </li>
+                  <li>
+                    <img src={LocationSvg} alt="" />
+                    <p>Baku, Azerbaijan</p>
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="AboutCMe-right">
               <img src={Avatar} alt="" />
             </div>
           </div>
-          <div className="AboutCSkills">asd</div>
+          <div className="AboutCSkills">
+            <div className="AboutCSkills-name">Skills</div>
+            <div className="AboutCSkills-skill">asd</div>
+            <div className="AboutCSkills-skill">asd</div>
+            <div className="AboutCSkills-skill">asd</div>
+          </div>
           <div className="AboutCComments">asd</div>
         </div>
       </div>
